152f4aa99668f00301408cc9ce829896445652db
Mo-ammar95 <mohamed.a.alhaj95@gmail.com>
2023-09-08 14:01:55 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints a string of char.
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
